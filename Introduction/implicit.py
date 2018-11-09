d5b66ccaa0eba54e7d8f3805f4f845c0690445b5
Bharath <windos.sai.user@gmail.com>
2018-11-09 15:54:37 +0530

    Intro

---
+++ Introduction/implicit.py
@@ -0,0 +1,13 @@
+#implicit type casting
+
+num1=342
+str2=45.88
+print("the data type of num1 is ",type(num1))
+print("the data type of str2 is",type(str2))
+# no syntax for implicit type casting because it is automatically done
+
+print('!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!')
+num3_str4= num1+str2
+
+print("the sum of two numbers is ",num3_str4)
+print("the data type of num3_str4 is",type(num3_str4))
