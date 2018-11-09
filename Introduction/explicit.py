0c1a7746470cf237b4ae9ab819aaa4a535cfef65
Bharath <windos.sai.user@gmail.com>
2018-11-09 15:40:13 +0530

    Intro

---
+++ Introduction/explicit.py
@@ -0,0 +1,29 @@
+explicit type cating
+
+num=654
+str="432"
+print("the datatype of num is:" ,type(num))
+print("the data type of str is :",type(str)
+print(num+str)
+invalid syntax error
+
+print("*******************************************")
+
+print("program for explicit type casting in python")
+
+num=654
+str="432"
+print("the data type of num is",type(num))
+print("the data type of str",type(str))
+print("before type casting data types are below" )
+#syntax of type casting
+num_str=int(str)
+print("after type casting ")
+print("data type of num_str is",type(num_str))
+num1_str1=num_str+num
+print("sum of two members is",num1_str1)
+print("data type of num1_str1 is ",type(num1_str1))
+
+print("####################################################################3")
+
+
