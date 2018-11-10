b5483f4bd179a5b679888a3d3c4fdd448bc3dbce
Bharath <windos.sai.user@gmail.com>
2018-11-10 12:53:00 +0530

    Intro

---
+++ reserved words/keywords.py
@@ -0,0 +1,13 @@
+# in python some keywords are reserved that means we cant able to use the keywords as to declare varaible or to store
+values
+# around 33 keywords are there
+#they are
+
+    *true ,false ,while,switch,continue,none,break,try
+    *finally,raise,assert,from,import,class,def,as,pass,global
+    *nonlocal,lambda,del,with
+    *in,yieldand,or,nor,is
+
+to find keyword
+import keyword
+keyword.kwlist
