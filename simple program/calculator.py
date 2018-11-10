2076d7516dd10beec8ef8753772c76be26641f9f
Bharath <windos.sai.user@gmail.com>
2018-11-10 12:28:09 +0530

    Intro

---
+++ simple program/calculator.py
@@ -0,0 +1,14 @@
+int1=input('enter first number')
+int2=input('enter second number')
+
+plus = float(int1)+float(int2)
+minus = float(int1)-float(int2)
+multi = float(int1)*float(int2)
+div = float(int1)/float(int2)
+
+print('sum of two numbers is',int1,int2,plus)
+print('sub of two numbers is ',int1,int2,minus)
+print("multi of two numbers is ",int1,int2,multi)
+print("div of two numbers is ",int1,int2,div)
+exdiv =int (div)
+print("type casted division result:",exdiv)
