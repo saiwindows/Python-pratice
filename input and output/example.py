dd30b34d083150f9116d929828c3b1047da27ec6
Bharath <windos.sai.user@gmail.com>
2018-11-10 12:12:11 +0530

    Intro

---
+++ input and output/example.py
@@ -0,0 +1,9 @@
+#simple print function program
+
+print('sai','bharath','ravi','prem','naga','rohith','naresh','venkatesh')
+
+#sep== used for sepeartes the objects with string
+
+end == used to place string on the end of the objects
+
+print('sai','bharath','ravi','prem','naga','rohith','naresh','venkatesh',sep='45',end='@')
