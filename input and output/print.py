e600e3651474af7a797f4e9210ffd6446ef8b559
Bharath <windos.sai.user@gmail.com>
2018-11-10 11:58:12 +0530

    Intro

---
+++ input and output/print.py
@@ -0,0 +1,5 @@
+print('hello my name is {identity} ,and my age is {years}'.format(identity='sai bharath', years=21))
+
+#{}-> place holder and 0,1 represnts the index
+
+print('I love {1} and {0}'.format('biryani','movies'))
