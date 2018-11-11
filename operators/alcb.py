c016fec2c69ddbdf6d4289d2ea91f54e61b23780
Bharath <windos.sai.user@gmail.com>
2018-11-11 12:17:28 +0530

    Intro

---
+++ operators/alcb.py
@@ -0,0 +1,52 @@
+#operators
+
+#arthimatic operators
+
+#+,-,*,/,%,//,**
+
+sai =23
+bharath= 43
+
+print( 'sai and bharath',sai+bharath)
+E=sai - bharath
+print('E is {}'.format(E))
+print('sai and bharath',sai*bharath )
+print('sai and bharath ',sai/bharath)
+print('sai and bharath',sai//bharath)
+print('sai and bharath',sai**bharath)
+
+#comparsion operators
+
+# these operators are used to in conditions to compare with object behaviour and return true or false if true it will
+# execute the ststements in the loop otherwise return false and exit from the condition
+
+#they are <,>,<=,>=,==,!=
+
+w=21
+s=45
+print('w and s',w<s)
+print( 'w and s',w>s)
+print('w and s',w<=s)
+print( ' is w and s are equal',w>=s)
+print('w and s',w!=s)
+
+#logical operators
+
+w=True
+s=False
+
+print(w and s)
+print(w or s)
+print(not w)
+print(not s)
+
+#bitwise operator
+
+& bitwise and
+| bitwise or
+~ xor
+^ bitwise not
+
+
+
+
