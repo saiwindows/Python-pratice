786ee49564e94a067e65bfd578f105c8010ad2f1
Bharath <windos.sai.user@gmail.com>
2018-12-02 18:28:08 +0530

    methods in strings

---
+++ data structures/string functions/count.py
@@ -0,0 +1,7 @@
+string="python is an case sensitive language"
+substring='case'
+count=string.count(substring)
+if string!=substring:
+    print("count is ",count)
+else:
+    print("count invalid")
--- praticeprograms/factorial.py
+++ praticeprograms/factorial.py
@@ -11,6 +11,6 @@ if factnum < 0:
 elif factnum==0:
     print(factnum," is 1")
 else:
-    for i in range(1,factnum+1):
-        factorail=factorail*i
+    for i in range(1,factnum+1):# factnum is used for increments i value same as given in input
+        factorail=factorail*i  # to multipy
         print("the factorail of ",factnum ,"is",factorail )
