c8368d18bb062e139c733944258711cc9882c2ee
Andrew Lane <AndrewLane@users.noreply.github.com>
2019-10-25 16:44:01 -0400

    Spellcheck

--- praticeprograms/factorial.py
+++ praticeprograms/factorial.py
@@ -1,16 +1,16 @@
 #algorithm
 #tep1: take input from the user
 #step2: multiply the input with previous number to input
-#step3:print the factorail of the input
+#step3:print the factorial of the input
 
 
 factnum=int(input("enter a factorial numberfact"))
-factorail=1
+factorial=1
 if factnum < 0:
-    print(factnum,"no factorail for this number")
+    print(factnum,"no factorial for this number")
 elif factnum==0:
     print(factnum," is 1")
 else:
     for i in range(1,factnum+1):# factnum is used for increments i value same as given in input
-        factorail=factorail*i  # to multipy
-        print("the factorail of ",factnum ,"is",factorail )
+        factorial=factorial*i  # to multipy
+        print("the factorial of ",factnum ,"is",factorial )
